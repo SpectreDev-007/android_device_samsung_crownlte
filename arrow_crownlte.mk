44472db89a618172d59a8c00d8a693f518efe4ce
baddar90 <mnbaddar1990@gmail.com>
2021-12-10 19:12:31 +0000

    use pixel XL fp
    * This allow using some of samsung apps

--- arrow_crownlte.mk
+++ arrow_crownlte.mk
@@ -39,12 +39,12 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 # ArrowOS Properties
 DEVICE_MAINTAINER := Baddar90
 
-BUILD_FINGERPRINT := "samsung/crownltexx/crownlte:10/QP1A.190711.020/N960FXXU6FTK1:user/release-keys"
+BUILD_FINGERPRINT := "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=crownltexx \
-    PRIVATE_BUILD_DESC="crownltexx-user 10 QP1A.190711.020 N960FXXU6FTK1 release-keys"
+    PRIVATE_BUILD_DESC="marlin-user 10 QP1A.191005.007.A3 5972272 release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.PDA=N960FXXU6FTK1 \
-    ro.build.fingerprint=samsung/crownltexx/crownlte:10/QP1A.190711.020/N960FXXU6FTK1:user/release-keys
+    ro.build.fingerprint=google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys
